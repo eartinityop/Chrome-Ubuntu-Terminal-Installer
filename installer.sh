e5ca90a74b8e325598bf75c777aa265c9980bf0d
Eartinity <77088825+eartinityop@users.noreply.github.com>
2024-03-01 10:53:01 +0530

    Create installer.sh

---
+++ installer.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Update Packages and Install wget
+apt update && apt install wget -y
+
+# Download Google-Chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+
+# Install Google-Chrome
+dpkg -i google-chrome-stable_current_amd64.deb
+
+# Fix the errors faced (if any)
+apt-get install -f
+
+# Launch Chrome (for root users only)
+google-chrome --no-sandbox --user-data-dir
